bd56f024e7bb18dcfd40afcba6cbb9c36a860862
Jorge Moreira <jmoreira@linkux-it.com>
2019-12-06 13:25:46 -0400

    Fix lotes service

--- server/routes/land.js
+++ server/routes/land.js
@@ -5,6 +5,8 @@ const LandController = require('../controllers/land');
 
 router.get('/', LandController.findAll);
 router.post('/', LandController.store);
+router.get('/intersect', LandController.intersect);
+router.get('/select', LandController.select);
 router.get('/:id', LandController.lookup, LandController.get);
 router.put('/:id', LandController.lookup, LandController.update);
 router.delete('/:id', LandController.lookup, LandController.remove);
