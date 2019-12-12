73893751a1626182810a9a5c06420482a355cd50
Jorge Moreira <jmoreira@linkux-it.com>
2019-12-12 15:41:27 -0400

    Remove unnecesary reducers

--- app/redux/index.js
+++ app/redux/index.js
@@ -3,12 +3,10 @@ import { combineReducers } from 'redux';
 /* Import reducers and combine them below */
 import example from './reducers/example';
 import map from './reducers/MapReducer';
-import registerWizard from './reducers/register-wizard';
 
 const store = combineReducers({
   example,
-  map,
-  registerWizard
+  map
 });
 
 export default store;
