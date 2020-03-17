02959069553c3058de62e58d468c437ada36e61c
rocaiguina <lrocaiguina@gmail.com>
2020-03-17 12:29:30 -0400

    fix module import error

--- app/components/map-view/Toolbar.js
+++ app/components/map-view/Toolbar.js
@@ -2,41 +2,39 @@ import React from 'react';
 import { Popover, Radio, Col, Row } from 'antd';
 import Button from '../ui/Button';
 import Icon from '../ui/Icon';
-import Menu from '../intro/Menu';
+import Menu from '../intro/menu';
 
 class MapToolBar extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            mostrar: false
-        };
-    }
-    
-    closeOverlay = (event) => {
-        this.setState(
-            {
-                mostrar: false
-            }
-        )
-    }    
-    openOverlay = (event) => {
-        this.setState(
-            {
-                mostrar: true
-            }
-        )
-    }    
-    
-    userLogin = (event) => {
-        window.location.href = '/user/login'; 
-    }
-    
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mostrar: false,
+    };
+  }
+
+  closeOverlay = event => {
+    this.setState({
+      mostrar: false,
+    });
+  };
+  openOverlay = event => {
+    this.setState({
+      mostrar: true,
+    });
+  };
+
+  userLogin = event => {
+    window.location.href = '/user/login';
+  };
+
+  render() {
     const RadioMapView = (
-      <Radio.Group onChange={this.props.onChangeModeView} value={this.props.mapView}
+      <Radio.Group
+        onChange={this.props.onChangeModeView}
+        value={this.props.mapView}
         size="large"
-        className="popeditblack">
+        className="popeditblack"
+      >
         <Row>
           <Radio value="list">Ver Lista</Radio>
         </Row>
@@ -47,53 +45,90 @@ class MapToolBar extends React.Component {
     );
 
     const RadioAreaView = (
-      <Radio.Group onChange={this.props.onChangeAreaView} value={this.props.areaView}
+      <Radio.Group
+        onChange={this.props.onChangeAreaView}
+        value={this.props.areaView}
         className="popeditblack"
-        size="large">
+        size="large"
+      >
         <Row>
-          <Radio value="proposed">Areas Naturales<br/>Propuestas</Radio>
+          <Radio value="proposed">
+            Areas Naturales
+            <br />
+            Propuestas
+          </Radio>
         </Row>
         <Row>
-          <Radio value="conserved">Areas Naturales<br/>Protegidas</Radio>
+          <Radio value="conserved">
+            Areas Naturales
+            <br />
+            Protegidas
+          </Radio>
         </Row>
       </Radio.Group>
     );
 
     return (
       <div className="toolbar toolbar-menu">
-          {this.state.mostrar ? ( 
-          <div className={"overlaymenu"}>
-            <div className={"buttonmenu"}>
-                <Button type="primary" ghost size="large" onClick={this.userLogin} >
-                    <Icon type="user" />
-                </Button>
-                <Button style={{marginLeft:"10px"}} type="primary" ghost size="large" onClick={this.closeOverlay}>
-                    <Icon type="close" />
-                </Button>
+        {this.state.mostrar ? (
+          <div className={'overlaymenu'}>
+            <div className={'buttonmenu'}>
+              <Button
+                type="primary"
+                ghost
+                size="large"
+                onClick={this.userLogin}
+              >
+                <Icon type="user" />
+              </Button>
+              <Button
+                style={{ marginLeft: '10px' }}
+                type="primary"
+                ghost
+                size="large"
+                onClick={this.closeOverlay}
+              >
+                <Icon type="close" />
+              </Button>
             </div>
-            <Menu/>
-            <div className={"menubottom"}>
-                <div>
-                <Button type="simplebtn2">Visitar página <Icon type="arrow-right" /></Button>
+            <Menu />
+            <div className={'menubottom'}>
+              <div>
+                <Button type="simplebtn2">
+                  Visitar página <Icon type="arrow-right" />
+                </Button>
                 <img src="/images/naturaleza.png" className="img-responsive" />
               </div>
             </div>
-        </div>): null}
+          </div>
+        ) : null}
         <ul>
           <li>
-            <Button ghost onClick={this.openOverlay} ><Icon type="menu" /></Button>
+            <Button ghost onClick={this.openOverlay}>
+              <Icon type="menu" />
+            </Button>
           </li>
           <li>
-            <Button ghost><Icon type="user" /></Button>
+            <Button ghost>
+              <Icon type="user" />
+            </Button>
           </li>
           <li>
             <Popover content={RadioMapView} trigger="click" className="m33-btn">
-              <Button ghost><Icon type="eye" /></Button>
+              <Button ghost>
+                <Icon type="eye" />
+              </Button>
             </Popover>
           </li>
           <li>
-            <Popover content={RadioAreaView} trigger="click" className="m33-btn">
-              <Button ghost><Icon type="layers" /></Button>
+            <Popover
+              content={RadioAreaView}
+              trigger="click"
+              className="m33-btn"
+            >
+              <Button ghost>
+                <Icon type="layers" />
+              </Button>
             </Popover>
           </li>
         </ul>
