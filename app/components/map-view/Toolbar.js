82fd9c62a0b95fb3fc9defcd3a915bf9245d667e
Jorge Moreira <jmoreira@linkux-it.com>
2020-03-18 00:54:05 -0400

    Adds sidebar menu

--- app/components/map-view/Toolbar.js
+++ app/components/map-view/Toolbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Popover, Radio, Col, Row } from 'antd';
 import Button from '../ui/Button';
 import Icon from '../ui/Icon';
-import Menu from '../intro/Menu';
 
 class MapToolBar extends React.Component {
     
@@ -71,7 +70,6 @@ class MapToolBar extends React.Component {
                     <Icon type="close" />
                 </Button>
             </div>
-            <Menu/>
             <div className={"menubottom"}>
                 <div>
                 <Button type="simplebtn2">Visitar página <Icon type="arrow-right" /></Button>
