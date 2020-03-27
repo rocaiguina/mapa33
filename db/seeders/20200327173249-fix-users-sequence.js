4a80ca3f14988d67ddc65d021f694eea627714a5
Jorge Moreira <jmoreira@linkux-it.com>
2020-03-27 13:43:50 -0400

    Update land fields and seeders

---
+++ db/seeders/20200327173249-fix-users-sequence.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Model = require('../../db/models');
+const User = Model.User;
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return User.max('id')
+      .then(value => {
+        return queryInterface.sequelize
+          .query(`ALTER SEQUENCE "users_id_seq" RESTART WITH ${value}`);
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
