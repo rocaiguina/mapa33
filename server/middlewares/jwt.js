84b17dbd84d9049bb3f86401ecb3e908f043170f
Jorge Moreira <jmoreira@linkux-it.com>
2020-06-24 22:06:29 -0400

    Improve jwt authentication

--- server/middlewares/jwt.js
+++ server/middlewares/jwt.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const jwt = require('jsonwebtoken');
+const Models = require('../../db/models');
+const User = Models.User;
 
 module.exports = {
   verifyJWT: function(req, res, next) {
@@ -26,17 +28,24 @@ module.exports = {
     if (accessToken) {
       try {
         const decrypt = jwt.verify(accessToken, process.env.JWT_SECRET);
-        req.user = {
-          id: decrypt.id,
-          first_name: decrypt.first_name,
-          last_name: decrypt.last_name,
-          email: decrypt.email,
-        };
-        return next();
+        User.findOne({
+          where: { id: decrypt.id },
+        })
+          .then(function(user) {
+            if (!user) {
+              return res.status(401).send();
+            }
+            req.user = user.get({ plain: true });
+            next();
+          })
+          .catch(function(err) {
+            res.status(401).send(err);
+          });
       } catch (err) {
         res.status(401).send();
       }
+    } else {
+      res.status(401).send();
     }
-    res.status(401).send();
   },
 };
